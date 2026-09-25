e0695ddee1ebc3da3d18cf2e8b89310aca338069
be-the-root <cardexeditsyt@gmail.com>
2026-09-25 23:30:49 +0530

    made the text pixel and did some minor changes and going to make the game

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
     var btn = document.getElementById("continue-btn");
   var warn = document.getElementById("warning-screen");
  var inst = document.getElementById("instruction-screen");
+var btn2 = document.getElementById("continue-btn2");
 
     btn.onclick = function() {
       warn.style.display = "none";
     inst.style.display = "flex";
+    }
+
+    btn2.onclick = function() {
+    inst.style.display = "none";
     }
