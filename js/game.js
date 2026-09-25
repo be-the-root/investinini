4092b602c2b789a910ab915e46e21942f79ec940
be-the-root <cardexeditsyt@gmail.com>
2026-09-26 00:33:17 +0530

    i made the main  interface of the game

---
+++ js/game.js
@@ -0,0 +1,5 @@
+    console.log("bulb game ready");
+
+    var evidence = document.querySelector(".evidence-window p1");
+    var question = document.querySelector(".question-window p1");
+    var hint = document.querySelector(".hint-window p1");
