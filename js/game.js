876cc58ea918640498c32d919c21fe261f48a306
be-the-root <cardexeditsyt@gmail.com>
2026-09-26 20:37:32 +0530

    made my first game mechenic now i only have to write the hints on a real paper

--- js/game.js
+++ js/game.js
@@ -8,6 +8,15 @@
     var hintsBox = document.getElementById("hints-container");
     var clickCount = 0;
 
+    var imageViewer = document.getElementById("image-viewer");
+    var viewerImg = document.getElementById("viewer-img");
+    var backBtn = document.getElementById("back-btn");
+    var collectedHints = document.getElementById("collected-hints");
+    var hintText = document.getElementById("hint-text");
+
+    var currentHint = null;
+    var collectedCount = 0;
+
     lightBtn.onclick = function() {
       clickCount++;
       if (clickCount == 10) {
@@ -17,6 +26,43 @@
               document.body.classList.remove("flash");
               lightBtn.style.display = "none";
               hintsBox.style.display = "flex";
+              hintText.innerText = "analyze the hints";
           }, 10);
       }
+    }
+
+    var openImage = function(btn, imgName) {
+        console.log("trying to open: " + imgName);
+        imageViewer.style.display = "flex";
+        viewerImg.src = imgName;
+        currentHint = btn;
+    }
+
+    document.getElementById("hint1").onclick = function() {
+        openImage(this, "image1.png");
+    }
+
+    document.getElementById("hint2").onclick = function() {
+        openImage(this, "image2.png");
+    }
+
+    document.getElementById("hint3").onclick = function() {
+        openImage(this, "image3.png");
+    }
+
+    document.getElementById("hint4").onclick = function() {
+        openImage(this, "image4.png");
+    }
+
+    backBtn.onclick = function() {
+        imageViewer.style.display = "none";
+        
+        if (currentHint.parentNode.id === "hints-container") {
+            collectedHints.appendChild(currentHint);
+            collectedCount++;
+            
+            if (collectedCount == 4) {
+                hintsBox.style.display = "none";
+            }
+        }
     }
